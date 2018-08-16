746226ae46d53c18233cb802aded3dc87b60d3c0
mufasa1976 <mufasa1976@coolstuff.software>
2018-08-16 05:51:02 +0200

    x

--- src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/AuthorizationServerConfiguration.java
@@ -1,17 +1,30 @@
 package io.github.mufasa1976.spring.oauth2.authenticationserver.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+  private final AuthenticationManager authenticationManager;
+  private final PasswordEncoder passwordEncoder;
+
   @Override
   public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    security.passwordEncoder(passwordEncoder);
   }
 
   @Override
@@ -20,5 +33,28 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints.tokenStore(tokenStore())
+             .tokenEnhancer(tokenEnhancer())
+             .accessTokenConverter(accessTokenConverter())
+             .authenticationManager(authenticationManager);
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+    accessTokenConverter.setSigningKey("123");
+    return accessTokenConverter;
+  }
+
+  @Bean
+  public TokenEnhancer tokenEnhancer() {
+    return (accessToken, authentication) -> {
+      return accessToken;
+    };
   }
 }
---
+++ src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package io.github.mufasa1976.spring.oauth2.authenticationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("user")
+        .password("user")
+        .roles("USER1", "USER2");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requestMatcher(new NegatedRequestMatcher(new AntPathRequestMatcher("/oauth/**")))
+        .authorizeRequests()
+        .anyRequest().permitAll();
+  }
+}
