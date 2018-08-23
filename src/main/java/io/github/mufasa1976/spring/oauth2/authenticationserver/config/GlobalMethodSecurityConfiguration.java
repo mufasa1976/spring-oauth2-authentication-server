7851201e48a27d1f690bf48df2e2b3c5487b461d
mufasa1976 <mufasa1976@coolstuff.software>
2018-08-23 08:59:27 +0200

    x

--- src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/GlobalMethodSecurityConfiguration.java
+++ src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/GlobalMethodSecurityConfiguration.java
@@ -2,28 +2,21 @@ package io.github.mufasa1976.spring.oauth2.authenticationserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
-@EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class GlobalMethodSecurityConfiguration
     extends org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration {
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    PasswordEncoder passwordEncoder = passwordEncoder();
-    auth.inMemoryAuthentication()
-        .withUser("user")
-        .password(passwordEncoder.encode("password"))
-        .roles("USER1", "USER2");
+  @Bean
+  public MethodSecurityExpressionHandler oauth2MethodSecurityExpressionHandler() {
+    return new OAuth2MethodSecurityExpressionHandler();
   }
 }
--- src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/WebSecurityConfiguration.java
+++ src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/config/WebSecurityConfiguration.java
@@ -2,12 +2,33 @@ package io.github.mufasa1976.spring.oauth2.authenticationserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity(debug = true)
+@Order
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder passwordEncoder = passwordEncoder();
+    auth.inMemoryAuthentication()
+        .withUser("user")
+        .password(passwordEncoder.encode("password"))
+        .roles("USER1", "USER2");
+  }
+
   @Bean
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
