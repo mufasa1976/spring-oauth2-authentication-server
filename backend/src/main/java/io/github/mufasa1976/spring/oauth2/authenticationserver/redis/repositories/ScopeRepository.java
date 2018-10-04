ce4bc7a37e768661d8e1dc5f41ac2204b76a2758
mufasa1976 <mufasa1976@coolstuff.software>
2018-10-04 21:30:59 +0200

    removed scope and repository

--- backend/src/main/java/io/github/mufasa1976/spring/oauth2/authenticationserver/redis/repositories/ScopeRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.mufasa1976.spring.oauth2.authenticationserver.redis.repositories;
-
-import io.github.mufasa1976.spring.oauth2.authenticationserver.redis.model.Scope;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ScopeRepository extends CrudRepository<Scope, String> {}
